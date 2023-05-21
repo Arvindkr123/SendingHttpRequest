18a09fe05f51df21b2f4be4bca6e545fe87dea0d
Arvind <thakurarvindkr10@gmail.com>
2023-05-21 11:16:09 +0530

    Making apps error proof

--- App.jsx
+++ App.jsx
@@ -1,43 +1,65 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState , useEffect} from 'react'
 import './App.css'
 import MoviesList from './component/MoviesList.jsx'
-import {VscLoading} from 'react-icons/vsc'
+import { VscLoading } from 'react-icons/vsc'
 
-const App = () => {
+function App() {
     const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const fetchMoviesHandler = async () => {
-        setLoading(true);
-        const response = await fetch('https://swapi.dev/api/films/');
-        const data = await response.json();
- 
-        const transformedMovies = data.results.map((movieData) => {
-            return {
-                id: movieData.episode_id,
-                title: movieData.title,
-                releaseDate: movieData.release_date,
-                openingText: movieData.opening_crawl
-            }
-        })
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-        setMovies(transformedMovies);
-        setLoading(false);
+    const fetchMoviesHandler = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('https://swapi.dev/api/films/');
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const data = await response.json();
+
+            const transformedMovies = data.results.map((movieData) => {
+                return {
+                    id: movieData.episode_id,
+                    title: movieData.title,
+                    openingText: movieData.opening_crawl,
+                    releaseDate: movieData.release_date,
+                };
+            });
+            setMovies(transformedMovies);
+        } catch (error) {
+            setError(error.message);
+        }
+        setIsLoading(false);
+    }, []);
+
+    useEffect(() => {
+        fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
+
+    let content = <p>Found no movies.</p>;
+
+    if (movies.length > 0) {
+        content = <MoviesList movies={movies} />;
     }
 
+    if (error) {
+        content = <p>{error}</p>;
+    }
 
+    if (isLoading) {
+        content = <p>Loading...</p>;
+    }
 
     return (
         <React.Fragment>
             <section>
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
-            <section>
-                {!loading && movies.length > 0 &&  <MoviesList movies={movies} />}
-                {!loading && movies.length === 0 &&  <p>No Movies found</p>}
-                {loading && <VscLoading size={'40px'}/>}
-            </section>
+            <section>{content}</section>
         </React.Fragment>
-    )
+    );
 }
 
 export default App;
